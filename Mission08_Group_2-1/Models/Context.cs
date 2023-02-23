db4c724fe7cb1008f251bc36dc1135d0bcf5044b
Dalyn Baldwin <101284161+dbdubz@users.noreply.github.com>
2023-02-23 15:11:47 -0700

    Prep to setup db

---
+++ Mission08_Group_2-1/Models/Context.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mission08_Group_2_1.Models
+{
+    public class Context : DbContext
+    {
+
+    }
+}
--- Mission08_Group_2-1/Startup.cs
+++ Mission08_Group_2-1/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Mission08_Group_2_1.Models;
 
 namespace Mission08_Group_2_1
 {
