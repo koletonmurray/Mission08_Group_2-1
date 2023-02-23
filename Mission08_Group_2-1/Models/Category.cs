5d4b76c5cc342f58bdefb29f3a0a30582d5cbf34
Dalyn Baldwin <101284161+dbdubz@users.noreply.github.com>
2023-02-23 15:06:51 -0700

    created models

---
+++ Mission08_Group_2-1/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission08_Group_2_1.Models
+{
+    public class Category
+    {
+        [Key][Required]public int Id { get; set; }
+        [Required]public string Name { get; set; }
+    }
+}
--- Mission08_Group_2-1/Models/Task.cs
+++ Mission08_Group_2-1/Models/Task.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Mission08_Group_2_1.Models
 {
     public class Task
     {
+        [Key][Required]public int Id { get; set; }
+        public DateTime DueDate { get; set; }
+        [Required]public int Quadrant { get; set; }
+        [Required]public int CategoryId { get; set; }
+        public Category TaskCategory { get; set; }
+        public bool Completed { get; set; }
     }
 }
