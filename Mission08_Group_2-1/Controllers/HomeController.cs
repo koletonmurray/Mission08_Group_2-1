3368e833e2af17bf34c10880d4eb195c56d5f8da
Koleton Murray <koletonm@byu.edu>
2023-02-23 13:32:52 -0700

    Add project files.

---
+++ Mission08_Group_2-1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission08_Group_2_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission08_Group_2_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission08_Group_2-1/Models/OurModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission08_Group_2_1.Models
+{
+    public class OurModel
+    {
+    }
+}
